1f1f54611bcb5b269ccf217ed1477f6058b87e5e
farhad <farhadhossain0085@gmail.com>
2020-05-14 23:43:18 +0600

    User login redirect added

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from app.models import Post, User
 from app.form import RegistrationForm, LoginForm
 from app import app, db, bcrypt
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     {
@@ -58,7 +58,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash("Login Failed! Please check your Email and Password", 'danger')
     return render_template("login.html", title="User Login", form=form)
@@ -67,3 +68,8 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title="User account")
