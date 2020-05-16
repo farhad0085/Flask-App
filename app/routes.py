5e19894e39869bbed6e3d455f2ee7401deb9846b
farhad <farhadhossain0085@gmail.com>
2020-05-16 22:23:57 +0600

    New Post creation added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from app.models import Post, User
-from app.form import RegistrationForm, LoginForm, UpdateAccountForm
+from app.form import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 import secrets
@@ -108,3 +108,15 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title="User account",
                            image_file=image_file, form=form)
+
+@app.route('/post/new', methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post has been created!", 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
