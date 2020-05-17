3ace866d35ba0ac3f6c63e560d90cc52adff242d
farhad <farhadhossain0085@gmail.com>
2020-05-17 21:01:03 +0600

    config added

--- app/__init__.py
+++ app/__init__.py
@@ -3,31 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+from app.config import Config
 
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "ac24802fb9a07239ef77af1ac2995fa4"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+mail = Mail()
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASS')
-
-mail = Mail(app)
-
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-from app.users.routes import users
-from app.main.routes import main
-from app.posts.routes import posts
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-app.register_blueprint(users)
-app.register_blueprint(main)
-app.register_blueprint(posts)
+    mail.init_app(app)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from app.users.routes import users
+    from app.main.routes import main
+    from app.posts.routes import posts
+
+    app.register_blueprint(users)
+    app.register_blueprint(main)
+    app.register_blueprint(posts)
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = "ac24802fb9a07239ef77af1ac2995fa4"
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
